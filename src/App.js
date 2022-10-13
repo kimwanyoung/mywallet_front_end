b017305ded71a432fad36154429ab10914f0bf13
김완영 <kwy@KWY.local>
2022-10-13 17:42:27 +0900

    ADD : 초기세팅 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './GlobalStyles';
+import styled from 'styled-components';
+import { Fragment } from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <GlobalStyles />
+      <Div>sssssss</Div>
+    </Fragment>
   );
-}
+};
 
 export default App;
+
+const Div = styled.h1`
+  border: 1px solid black;
+`;
